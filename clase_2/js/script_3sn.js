5dddbed45ad16ff4186d6ba98bb11631fb78c099
Jorge Rubiano <jorge.rubiano@ymail.com>
2014-08-15 20:01:26 -0500

    Clase_02
    Transiciones CSS, uso de Animate.css

---
+++ clase_2/js/script_3sn.js
@@ -0,0 +1,21 @@
+/*
+Miércoles laboratorio Sprites...
+Invesrigación 
+Animación
+*/
+window.onload = function()
+{
+	nom_div("ejecuta").addEventListener('click', function(event)
+	{
+		var objAnima = nom_div("imagen");
+		objAnima.setAttribute("class", "animated " + nom_div("tipoanima").value);
+		/*
+		objAnima = nom_div("animar");
+		objAnima.setAttribute("class", "animated " + nom_div("tipoanima").value);
+		*/
+	});
+	function nom_div(div)
+	{
+		return document.getElementById(div);
+	}
+}
